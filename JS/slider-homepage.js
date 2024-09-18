c971d608a5b1bbddf65e7e7f7f0b22db3d23949f
IlnytskyiBohdan <neklz1@icloud.com>
2024-09-18 05:18:32 +0300

    17.09

--- JS/slider-homepage.js
+++ JS/slider-homepage.js
@@ -6,16 +6,7 @@ $(document).ready(function () {
       cssEase: "liner",
       arrows: false,
       slidesToShow: 1,
-      slidesToScroll: 2,
+      slidesToScroll: 1,
       mobileFirst: true,
-      responsive: [
-        {
-          breakpoint: 767,
-          settings: {
-            slidesToShow: 4,
-            slidesToScroll: 3,
-          },
-        },
-      ],
     });
   });
